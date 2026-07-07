9906c6922ab95ec58c17c0a589056fcf2d6edda4
sebastianmichael94 <sebastian.michael94@gmail.com>
2026-07-06 22:44:55 -0400

    fix: resolve condition fallback block for database engines

--- classpulse_api/classpulse_config/settings.py
+++ classpulse_api/classpulse_config/settings.py
@@ -84,13 +84,20 @@ WSGI_APPLICATION = "classpulse_config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{os.path.join(str(BASE_DIR), 'db.sqlite3')}",
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
+if os.getenv("DATABASE_URL"):
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
